61a6452203371e104c994f7ae289d6e6a75b422c
Hristijan Petreski <hristijanpetreski@outlook.com>
2026-02-22 01:41:29 +0100

    Add cva, update biome and reformat code

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,23 @@
-import { MetaProvider, Title } from "@solidjs/meta";
+import { Meta, MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
 import { Suspense } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  return (
-    <Router
-      root={(props) => (
-        <MetaProvider>
-          <Title>SolidStart - Basic</Title>
-          <Suspense>{props.children}</Suspense>
-        </MetaProvider>
-      )}
-    >
-      <FileRoutes />
-    </Router>
-  );
+    return (
+        <Router
+            root={(props) => (
+                <MetaProvider>
+                    <Title>
+                        SalarySense - Full Salary Insight and Reporting
+                    </Title>
+                    <Meta name="theme-color" content="#006a6a" />
+                    <Suspense>{props.children}</Suspense>
+                </MetaProvider>
+            )}
+        >
+            <FileRoutes />
+        </Router>
+    );
 }
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,5 @@
 // @refresh reload
 import { mount, StartClient } from "@solidjs/start/client";
 
+// biome-ignore lint: the app container will be created in the final output
 mount(() => <StartClient />, document.getElementById("app")!);
